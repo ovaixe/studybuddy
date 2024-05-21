cfec54a86630c3e6a700e0cca9abb7134fb54304
Bhat Owais <owaisbhat996@outlook.com>
2024-05-21 15:11:19 +0530

    Added base app and templates

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -21,6 +21,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'base.apps.BaseConfig'
 ]
 
 MIDDLEWARE = [
@@ -38,7 +40,9 @@ ROOT_URLCONF = 'studybuddy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
