6f7904d5c96a680c90f5d12e87b553ad6fa43db0
Bhat Owais <owaisbhat996@outlook.com>
2024-05-25 12:09:28 +0530

    Updated index.js

--- static/js/index.js
+++ static/js/index.js
@@ -100,6 +100,18 @@ function toogle(element) {
   }
 }
 
+function toogleHide(deviceWidth, element, hide = True) {
+  if (element) {
+    if (deviceWidth < 900) {
+      if (hide) element.style.display = "none";
+      else element.style.display = "flex";
+    } else {
+      if (hide) element.style.display = "flex";
+      else element.style.display = "none";
+    }
+  }
+}
+
 function media() {
   let deviceWidth = window.innerWidth;
 
@@ -110,46 +122,26 @@ function media() {
   let profileRegister = document.getElementById("profile-register");
   let profileMenuButton = document.getElementById("profile-menuButton");
   let navSearchBar = document.getElementById("nav-searchbar");
-  let mainSearchBar = document.getElementById("searchbar");
+  let searchBar = document.getElementById("searchbar");
   let topicsSidebar = document.getElementById("topics-sidebar");
   let activitySidebar = document.getElementById("activity-sidebar");
-  let participants = document.getElementById("participants");
-  let roomSearchbar = document.getElementById("searchbar");
+  let participantsSidebar = document.getElementById("participants-sidebar");
   let browseButton = document.getElementById("browse-button");
   let activityButton = document.getElementById("activity-button");
   let participantsButton = document.getElementById("participants-button");
 
-  if (deviceWidth < 900) {
-    logoName.style.display = "none";
-    if (profileUsername) profileUsername.style.display = "none";
-    if (profileLogout) profileLogout.style.display = "none";
-    if (profileLogin) profileLogin.style.display = "none";
-    if (profileRegister) profileRegister.style.display = "none";
-    if (profileMenuButton) profileMenuButton.style.display = "flex";
-    if (navSearchBar) navSearchBar.style.display = "none";
-    if (mainSearchBar) mainSearchBar.style.display = "flex";
-    if (topicsSidebar) topicsSidebar.style.display = "none";
-    if (activitySidebar) activitySidebar.style.display = "none";
-    if (participants) participants.style.display = "none";
-    if (roomSearchbar) roomSearchbar.style.display = "flex";
-    if (browseButton) browseButton.style.display = "flex";
-    if (activityButton) activityButton.style.display = "flex";
-    if (participantsButton) participantsButton.style.display = "flex";
-  } else {
-    logoName.style.display = "flex";
-    if (profileUsername) profileUsername.style.display = "flex";
-    if (profileLogout) profileLogout.style.display = "flex";
-    if (profileLogin) profileLogin.style.display = "flex";
-    if (profileRegister) profileRegister.style.display = "flex";
-    if (profileMenuButton) profileMenuButton.style.display = "none";
-    if (navSearchBar) navSearchBar.style.display = "flex";
-    if (mainSearchBar) mainSearchBar.style.display = "none";
-    if (topicsSidebar) topicsSidebar.style.display = "flex";
-    if (activitySidebar) activitySidebar.style.display = "flex";
-    if (participants) participants.style.display = "flex";
-    if (roomSearchbar) roomSearchbar.style.display = "none";
-    if (browseButton) browseButton.style.display = "none";
-    if (activityButton) activityButton.style.display = "none";
-    if (participantsButton) participantsButton.style.display = "none";
-  }
+  toogleHide(deviceWidth, logoName, true);
+  toogleHide(deviceWidth, profileUsername, true);
+  toogleHide(deviceWidth, profileLogout, true);
+  toogleHide(deviceWidth, profileLogin, true);
+  toogleHide(deviceWidth, profileRegister, true);
+  toogleHide(deviceWidth, profileMenuButton, false);
+  toogleHide(deviceWidth, navSearchBar, true);
+  toogleHide(deviceWidth, searchBar, false);
+  toogleHide(deviceWidth, topicsSidebar, true);
+  toogleHide(deviceWidth, activitySidebar, true);
+  toogleHide(deviceWidth, participantsSidebar, true);
+  toogleHide(deviceWidth, browseButton, false);
+  toogleHide(deviceWidth, activityButton, false);
+  toogleHide(deviceWidth, participantsButton, false);
 }
